5bdd725b47159fa10ef1020e1f50b9f0e172f253
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2016-12-28 11:52:11 +0100

    Second commit
    Test github

--- point.h
+++ point.h
@@ -1,11 +1,19 @@
 #ifndef POINT_H
 #define POINT_H
-
+#include "constants.h"
 
 class Point
 {
 public:
+    real x;
+    real y;
+public:
     Point();
+
+    Point(real x, real y);
+
+    Point operator+ (const Point& p, const Vector& v);
+
 };
 
 #endif // POINT_H
