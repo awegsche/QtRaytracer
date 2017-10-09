4eaa0d545352e9b671a43a7f4c471b8865a377a7
awegsche <a.wegscheider7141@gmail.com>
2017-10-09 08:26:50 +0200

    Starting to polish some code files

--- Lights/light.h
+++ Lights/light.h
@@ -10,6 +10,7 @@ class ShadeRec;
 #ifdef WCUDA
 class ShadeRecCUDA;
 
+// Device Light class.
 class LightCUDA {
 public:
 	virtual __device__ CUDAreal3 L(ShadeRecCUDA& sr) = 0;
@@ -28,17 +29,24 @@ bool __inline__ __device__ LightCUDA::casts_shadows() {
 #endif // WCUDA
 
 
+// Represents a light in the scene. Responsible for direct illumination and shadows.
 class Light
 {
 
 public:
     Light();
 
+    // Returns the direction in which the light can be seen starting from the current hit point.
+    // For light sources like spherical light this will be sampled.
     virtual Vector get_direction(ShadeRec& sr) = 0;
+
+    // Returns the color of the light source as seen from the hitpoint.
     virtual RGBColor L(ShadeRec& sr) = 0;
 
     bool casts_shadows();
     void set_shadows(bool b);
+
+    // Returns true if the hit point lies in the shadow of this light source.
     virtual bool in_shadow(Ray& ray, ShadeRec& sr) = 0;
 
 protected:
